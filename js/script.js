dd0125a5a8ca65c3bf068fc3305ff33eaf89d2d6
Daniel Jones <vvsdan999@gmail.com>
2023-10-05 16:23:42 -0400

    add complete project from local

---
+++ js/script.js
@@ -0,0 +1,45 @@
+const input = document.getElementById("link-input");
+const linkForm = document.getElementById("link-form");
+const errMsg = document.getElementById("err-msg");
+
+linkForm.addEventListener("submit", formSubmit);
+
+//Validate a URL
+function validURL(str) {
+    var pattern = new RegExp(
+        "^(https?:\\/\\/)?" + // protocol
+            "((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|" + // domain name
+            "((\\d{1,3}\\.){3}\\d{1,3}))" + // OR ip (v4) address
+            "(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*" + // port and path
+            "(\\?[;&a-z\\d%_.~+=-]*)?" + // query string
+            "(\\#[-a-z\\d_]*)?$",
+        "i"
+    ); // fragment locator
+    return !!pattern.test(str);
+}
+
+function formSubmit(e) {
+    e.preventDefault();
+
+    if (input.value === "") {
+        errMsg.innerHTML = "Please enter something";
+        input.classList.add("border-red");
+    } else if (!validURL(input.value)) {
+        errMsg.innerHTML = "Please enter a valid URL";
+        input.classList.add("border-red");
+    } else {
+        errMsg.innerHTML = " ";
+        input.classList.remove("border-red");
+        alert("Success");
+    }
+}
+
+const btn = document.getElementById("menu-btn");
+const menu = document.getElementById("menu");
+
+btn.addEventListener("click", navToggle);
+function navToggle() {
+    btn.classList.toggle("open");
+    menu.classList.toggle("flex");
+    menu.classList.toggle("hidden");
+}
